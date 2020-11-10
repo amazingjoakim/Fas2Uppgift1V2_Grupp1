1c4ab9faa405008025e4f9301a1adc2fc4b9300b
Adnan Kadrispahic <adikadrispahic@gmail.com>
2020-11-10 10:00:03 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fas2Uppgift1V2_Grupp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Fas2 Uppgift 1 Version 2
+        }
+    }
+}
