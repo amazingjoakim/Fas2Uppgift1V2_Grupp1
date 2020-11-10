0e459e7a7210fc437e5d5ed2dc9d3969164d670f
Adnan Kadrispahic <adikadrispahic@gmail.com>
2020-11-10 12:19:10 +0100

    Working with commands

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,149 @@ namespace Fas2Uppgift1V2_Grupp1
 {
     class Program
     {
+        class TodoTask
+        {
+            public string date, state, description;
+
+            public TodoTask()
+            {
+
+            }
+            public TodoTask(string date, string state, string description)
+            {
+                this.date = date;
+                this.state = state;
+                this.description = description;
+            }
+        }
+
         static void Main(string[] args)
         {
-            //Fas2 Uppgift 1 Version 2
+            Console.WriteLine("Welcome to the todo list!\n" +
+                "Type 'quit' to quit, type 'help' for help!");
+
+            string command = "";
+            string[] commandWords;            
+            List<TodoTask> todoList = new List<TodoTask>();
+
+            do
+            {
+                Console.Write("> ");
+                command = Console.ReadLine();
+                commandWords = command.Split(' ');
+
+                if (command == "quit")
+                {
+                    Console.WriteLine("Bye");
+                }
+                else if (command == "help")
+                {
+                    PrintHelp();
+                }
+                else if (commandWords[0] == "load")
+                {
+                    string file = commandWords[1];
+                    todoList.AddRange(LoadFile(file));
+                }
+                else if (command == "show")
+                {
+                    int listPos = 1;
+                    foreach (TodoTask todo in todoList)
+                    {
+                        Console.WriteLine($"{listPos}. {todo.date} - {todo.state} - {todo.description}");
+                        listPos++;
+                    }
+                }
+                else if (commandWords[0] == "add")
+                {
+                    todoList.Add(AddTodoTask(commandWords));
+                }
+                else if (commandWords[0] == "delete")
+                {
+
+                    for (int i = 0; i < todoList.Count(); i++)
+                    {
+                        int listPos = i + 1;
+                        if (listPos == int.Parse(commandWords[1]))
+                        {
+                            todoList.RemoveAt(i);
+                        }
+                    }
+
+                }
+                else if (commandWords[0] == "set")
+                {
+                    if (commandWords[2] == "avklarad")
+                    {
+                        todoList[int.Parse(commandWords[1]) - 1].state = "*";
+                    }
+                    else if (commandWords[2] == "pågående")
+                    {
+                        todoList[int.Parse(commandWords[1]) - 1].state = "p";
+                    }
+                }
+                else if (commandWords[0] == "move")
+                {
+                    //todoList[int.Parse(commandWords[2]) - 2];
+
+                    if (commandWords[2] == "up")
+                    {
+                       //todoList[int.Parse(commandWords[2]) - 2];
+                    }
+
+                }
+                else if (command == "save")
+                {
+
+                }
+
+            } while (command.ToLower() != "quit");
+
         }
+
+        private static TodoTask AddTodoTask(string[] commandWords)
+        {
+            for (int i = 3; i < commandWords.Count(); i++)
+            {
+                commandWords[2] += $" {commandWords[i]}";
+            }
+
+            return new TodoTask(commandWords[1], "v", commandWords[2]);
+        }
+
+        static List<TodoTask> LoadFile(string filePath) //C:\Users\Admin\grupp1.lis
+        {
+            List<TodoTask> todoList = new List<TodoTask>();
+            string[] fileLine;
+
+            using(StreamReader sr = new StreamReader(@filePath))
+            {
+                while (sr.Peek() >= 0)
+                {
+                    fileLine = sr.ReadLine().Split('#');
+                    TodoTask newTask = new TodoTask(fileLine[0], fileLine[1], fileLine[2]);
+                    todoList.Add(newTask);
+                    Console.WriteLine($"{newTask.date} - {newTask.state} - {newTask.description}");
+                }
+            }
+
+            return todoList;
+        }
+
+        static void PrintHelp()
+        {
+            Console.WriteLine("Help commands: \n" +
+                "<load> <filepath> -- loads the todofile \n" +
+                "<add> <todoTask> -- add new todo task\n" +
+                "<delete> -- deletes todo from list\n" +
+                "<save> -- saves list to current file\n" +
+                "<save> <filepath> -- saves list to specific file path\n" +
+                "<move> <number> <up/down> -- moves todo task position in list\n" +
+                "<set> <taskNo> <state> -- set state for todo task\n" +
+                "<show> -- shows todo list"
+                );
+        }
+
+
     }
 }
